cac513710e1909279210779c6ffc4cd81ff30940
Vishal Garg <vishugarg10004@gmail.com>
2020-11-16 20:27:56 +0530

    [Vishal] added different array methods for getting different output

--- EmployeeWageLearningProblem.js
+++ EmployeeWageLearningProblem.js
@@ -1,7 +1,7 @@
 //welcome message
 console.log("welcome to employee wage computation problem");
 //UC1
-console.log("UC1");
+//console.log("UC1");
 //UC1
 /*{
 let checkForEmployeePresence= Math.floor((Math.random()*10)%2);
@@ -17,6 +17,29 @@ else
 }*/
 //UC2
 //console.log("UC2");
+
+//UC3
+function getEmpHours()
+{ 
+    let emp_hours=0;
+    //checking for employee presence using math floor and math random
+    let checkForEmployeePresence= Math.floor((Math.random()*10)%3);
+    //using switch case for choosing different emp hours
+    switch(checkForEmployeePresence)
+{
+    case 1:
+        //console.log("employee is present for part time");
+        emp_hours=4;
+        return emp_hours;
+    case 2:
+        //console.log("employee is present for full time");
+        emp_hours=8;
+        return emp_hours;
+    default:
+       // console.log("employee is absent");
+        return emp_hours;
+}
+}
 
 //declaring constants
 const IS_ABSENT=0;
@@ -43,29 +66,58 @@ totalEmpHours+=emp_hours;
 employeeWage= WAGE_PER_HOUR*totalEmpHours;
 }
 //printing values
-console.log(empDailyWageArray);
 console.log("total work hours "+totalEmpHours);
 console.log("totol emp wage "+employeeWage);
 console.log("total working days "+workingDay);
-//UC3
-function getEmpHours()
-{ 
-    let emp_hours=0;
-    //checking for employee presence using math floor and math random
-    let checkForEmployeePresence= Math.floor((Math.random()*10)%3);
-    //using switch case for choosing different emp hours
-    switch(checkForEmployeePresence)
+console.log(empDailyWageArray);
+//UC 7a- calculate total wage using array foreach traversal or reduce method
+let totalEmployeeWage=0
+function sum(dailyWage)
 {
-    case 1:
-        //console.log("employee is present for part time");
-        emp_hours=4;
-        return emp_hours;
-    case 2:
-        //console.log("employee is present for full time");
-        emp_hours=8;
-        return emp_hours;
-    default:
-       // console.log("employee is absent");
-        return emp_hours;
+   totalEmployeeWage+= dailyWage;
 }
+empDailyWageArray.forEach(sum);
+console.log("7a Emp wage with foreach: "+totalEmployeeWage);
+function totalWages(totalWage,dailyWage)
+{
+    return totalWage+dailyWage;
 }
+
+console.log("UC 7a- Emp wage with reduce: "+empDailyWageArray.reduce(totalWages,0));
+
+//UC 7B- show the day along with daily wage using array map helper functions.
+let workDayCount= 0;
+function mapDayWithWage(dailyWage)
+{
+    workDayCount++;
+    return workDayCount + " = "+ dailyWage; 
+}
+let mapDaywithWageArray= empDailyWageArray.map(mapDayWithWage);
+console.log("7B: "+ mapDaywithWageArray);
+//UC 7C- Show days when full time wage of 160 were earned
+function fulltimeWage(dailyWage)
+{
+    return dailyWage.includes("160");
+}
+let fullDayWageArray= mapDaywithWageArray.filter(fulltimeWage);
+console.log("7C: "+fullDayWageArray); 
+//UC 7D- finding first day when employee was present for full day
+console.log("7D first time employee was present for 1st time: "+ mapDaywithWageArray.find(fulltimeWage));
+//UC 7E- checking if every element of fullday wage array is truely holding 160 as employee wage.
+console.log("7E: checking if all elements of full time employee has wage of 160-  "+fullDayWageArray.every(fulltimeWage));
+//UC 7F- checking if there is any part time wage
+function checkPartTime(dailyWages)
+{
+    return dailyWages.includes('80');
+}
+console.log("7F- Checking if there is a employee with part time wage "+mapDaywithWageArray.some(checkPartTime));
+//UC 7G: finding out no of days employee actually worked
+function totalDaysWorked(noOfDays,dailyWage)
+{
+    if(dailyWage>0)
+    {
+        return noOfDays+=1;
+    }
+    return noOfDays;
+}
+console.log("7G: The no of days employee worked: "+empDailyWageArray.reduce(totalDaysWorked,0));
